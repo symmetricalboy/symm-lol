32fa0b58b6ae1dfcb3a298d84148306c2d9dab9e
symmetricalboy <dylan.g.singer@gmail.com>
2025-05-19 00:01:15 -0500

    Update route handling in server.cjs to match all paths with wildcard

--- server.cjs
+++ server.cjs
@@ -5,7 +5,7 @@ const port = process.env.PORT || 4173;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
